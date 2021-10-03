acd6426cc0209e393a94ec067b7dfed2f1f53a5c
heltongo <helton.goulart@griffithuni.edu.au>
2021-10-04 02:13:03 +1000

    Helton feature 4

--- feature4.py
+++ feature4.py
@@ -2,31 +2,58 @@
 import sqlite3
 from tabulate import tabulate
 
+
+def sql_data_to_list_of_dicts(path_to_db, select_query):
+    """Returns data from an SQL query as a list of dicts."""
+    try:
+        con = sqlite3.connect(path_to_db)
+        con.row_factory = sqlite3.Row
+        things = con.execute(select_query).fetchall()
+        unpacked = [{k: item[k] for k in item.keys()} for item in things]
+        return unpacked
+    except Exception as e:
+        print(f"Failed to execute. Query: {select_query}\n with error:\n{e}")
+        return []
+    finally:
+        con.close()
+
+def count_keys(dict):
+    count = 0
+    for i in enumerate(dict):
+        count += 1
+    return count
+
+
+
 def alcohol_Analysis():
-    list_column=[]
-    list_values=[]
-
-    connection = sqlite3.connect("Crashdb.db")
-    cursor = connection.cursor()
-    string = "SELECT * from CrashStatisticsVictoria \
-                WHERE ACCIDENT_DATE BETWEEN '2017-07-01' AND '2017-07-30'\
+    QUERY = "SELECT * from CrashStatisticsVictoria \
+                WHERE ACCIDENT_DATE BETWEEN '2014-01-01' AND '2018-12-31' AND ALCOHOLTIME='Yes'\
                 ORDER BY ACCIDENT_DATE, ACCIDENT_TIME"
-    string1 = "PRAGMA TABLE_INFO(CrashStatisticsVictoria)"
-    cursor.execute(string)
-    result = cursor.fetchall()
-    cursor.execute(string1)
-    result1 = cursor.fetchall()
+
+    result = sql_data_to_list_of_dicts("Crashdb.db", QUERY)
+
+    print("Total records: ",count_keys(result))
+    print("First record date: ",result[0].get('ACCIDENT_DATE'))
+    print("Last record date: ",result[24530].get('ACCIDENT_DATE'))
+
+#Finally  :D
 
 
-    for r in result1:
-        list_column.append(r[1])
+# STOP
+'''
+#def calculate_alcohol(records):
+    type_alcohol = calculate_type(lower_date=lower_date, higher_date=higher_date, crash_type='alcohol')
 
-    for r in result:
-        list_values.append(r)
-
-
-    print(tabulate(list_values,headers=list_column))
-    connection.close()
-
-alcohol_Analysis()
-exit()
+    for data in type_alcohol:
+        records = {
+            "gender": data['gender'],
+            "year": data['gender'],
+            "time": data['gender'],
+            "Age": data['gender'],
+            "Vehicle_Type": data['gender'],
+            "road_type": data['gender'],
+        }
+'''
+if __name__ == "__main__":
+    alcohol_Analysis()
+    exit()
